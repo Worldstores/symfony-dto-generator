1e4a7b877d6e5cca250b2dd9a0f51d4db6c2c606
Puraskar Sapkota <puraskar.sapkota@worldstores.co.uk>
2016-02-22 15:39:56 +0000

    added Bundle file

--- Command/GenerateDtoCommand.php
+++ Command/GenerateDtoCommand.php
@@ -74,26 +74,8 @@ class GenerateDtoCommand extends Server implements ContainerAwareInterface
         }
         
         $input->setArgument('type', $type);
-    	$input->setArgument('destination', $this->convertBundlePathToActualPath($input->getArgument('destination')));
-    	$input->setArgument('source', $this->convertBundlePathToActualPath($input->getArgument('source')));
+    	$input->setArgument('destination', $input->getArgument('destination'));
+    	$input->setArgument('source', $input->getArgument('source'));
     	return parent::execute($input, $output);
-    }
-    
-    /**
-     * Converts Bundles Path to Actual Path
-     * @param string $path
-     * @return string
-     */
-    protected function convertBundlePathToActualPath($path) 
-    {
-    	$bundleNameSeparatorPosition = strpos(':', $path);
-        
-        if ($bundleNameSeparatorPosition > 0) {
-            $bundleName = substr($path, 0, $bundleNameSeparatorPosition);
-            $bundle = $this->getContainer()->get("kernel")->getBundle($bundleName);
-            $bundlePath = $bundle->getPath(); 
-            return $bundlePath . substr($path, $bundleNameSeparatorPosition);
-        }
-        return $path;
     }
 }
---
+++ WsSysDtoGeneratorBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WsSys\DtoGeneratorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle Which Generates PHP Classes
+ */
+class WsSysDtoGeneratorBundle extends Bundle
+{
+    
+}
